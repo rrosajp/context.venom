40fbb0b5dd2a1f6b9f6f4ba2e349fd30d4a51495
Venom <46383815+123Venom@users.noreply.github.com>
2020-09-03 03:39:49 -0400

    v1.0.6

--- service.py
+++ service.py
@@ -34,9 +34,9 @@ class PropertiesUpdater(xbmc.Monitor):
 				xbmc.log('Context menu item disabled: {0}'.format(id),xbmc.LOGNOTICE)
 
 # start monitoring settings changes events
-xbmc.log('Context.Venom: service started', xbmc.LOGNOTICE)
+xbmc.log('[CONTEXT.VENOM]  service started', xbmc.LOGNOTICE)
 properties_monitor = PropertiesUpdater()
 
 # wait until abort is requested
 properties_monitor.waitForAbort()
-xbmc.log('Context.Venom: service stopped',xbmc.LOGNOTICE)
+xbmc.log('[CONTEXT.VENOM]  service stopped',xbmc.LOGNOTICE)
